03e9e4f88e6eaec3a0737b5a0fa4ca5370a6fb49
John Doe <lana.lakutina@mail.ru>
2022-03-09 15:55:49 +0300

    4 new practices test

---
+++ practice4/app/src/main/java/ru/mirea/lukutin/practice4/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.mirea.lukutin.practice4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
