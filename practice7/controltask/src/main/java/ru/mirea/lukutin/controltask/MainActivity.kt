58690f811903012d2bea86e9e26abce3380940a4
John Doe <lana.lakutina@mail.ru>
2022-04-07 19:43:08 +0300

    practise 7 completed

---
+++ practice7/controltask/src/main/java/ru/mirea/lukutin/controltask/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.mirea.lukutin.controltask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import ru.mirea.lukutin.firebaseauth.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+    }
+}
